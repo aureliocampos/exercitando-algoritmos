4c6b79bc811f614b3df32f41459f0eb211b46007
Aurélio Campos <aureliocampos.dev@gmail.com>
2022-05-18 13:49:02 -0300

    feat: Resolução Algoritmo 2 - C++

--- c++/c++/algoritmo-2.cpp
+++ c++/c++/algoritmo-2.cpp
@@ -1,3 +1,25 @@
 /**
-
+	2) Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boasvindas para ela:
+	Ex:
+	Qual é o seu nome? João da Silva
+	Olá João da Silva, é um prazer te conhecer!
 */
+
+#include <iostream>
+#include <locale>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "");
+	
+	string name = "";
+
+	cout << "\n" << "Qual o seu nome? " << "\n";
+	cin >> name;
+
+	cout << "\n" << "Olá " << name << ", é um prazer te conhecer!" << "\n\n";
+
+	return 0;
+}
